16a56a589df0a8224e78a397309fcaea4ce0f5af
sysfi <64483657+sysfi@users.noreply.github.com>
2023-03-21 16:35:11 +0100

    Update open_clip.py

--- clip_benchmark/models/open_clip.py
+++ clip_benchmark/models/open_clip.py
@@ -1,8 +1,19 @@
 import open_clip
-
+import torch
+from torch import nn
 
 def load_open_clip(model_name: str = "ViT-B-32-quickgelu", pretrained: str = "laion400m_e32", cache_dir: str = None, device="cpu"):
     model, _, transform = open_clip.create_model_and_transforms(model_name, pretrained=pretrained, cache_dir=cache_dir)
+    proj = nn.Linear(512, 128)
+    # Load state
+    model.transformer = nn.Sequential(
+        model.transformer,
+        proj
+    )
+    model.visual = nn.Sequential(
+        model.visual,
+        proj
+    )
     model = model.to(device)
     tokenizer = open_clip.get_tokenizer(model_name)
     return model, transform, tokenizer
