68956f3d2c0a9e7fe5ce1292d78bbc7fd4117963
sysfi <64483657+sysfi@users.noreply.github.com>
2023-03-21 19:29:41 +0100

    Update open_clip.py

--- clip_benchmark/models/open_clip.py
+++ clip_benchmark/models/open_clip.py
@@ -1,8 +1,11 @@
 import open_clip
-
+import torch
 
 def load_open_clip(model_name: str = "ViT-B-32-quickgelu", pretrained: str = "laion400m_e32", cache_dir: str = None, device="cpu"):
     model, _, transform = open_clip.create_model_and_transforms(model_name, pretrained=pretrained, cache_dir=cache_dir)
+    model = torch.quantization.quantize_dynamic(
+        model, dtype=torch.qint8
+    )
     model = model.to(device)
     tokenizer = open_clip.get_tokenizer(model_name)
     return model, transform, tokenizer
